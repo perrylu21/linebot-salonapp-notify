841ab43a9123cec76205e9c760b07de65c589237
perrylu21 <perrylu21@gmail.com>
2024-02-06 22:08:05 +0800

    first commit

---
+++ app_push.py
@@ -0,0 +1,89 @@
+import os
+from datetime import datetime
+
+from flask import Flask, abort, request, jsonify
+
+# https://github.com/line/line-bot-sdk-python
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+import configparser
+
+app = Flask(__name__)
+
+#line_bot_api = LineBotApi(os.environ.get("CHANNEL_ACCESS_TOKEN"))
+#handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
+#user_id = "U993e4bed50c6b80ac697b078fda84a01"
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
+handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
+user_id = config.get('line-bot', 'user_id')
+
+@app.route("/",methods=["GET"])
+def get_params():
+   
+    date_par = request.args.get('date')
+    time_par = request.args.get('time')
+    name_par = request.args.get('name')
+    srv_par = request.args.get('service')
+    #age = int(request.args.get('age'))
+    #timestr = convert_date_time(time_par)
+    #text_msg ="日期:" + date_par + "\n"+ \
+    #          "時間:" + time_par + "\n"+ \
+    #          "姓名:" + name_par + "\n" + \
+    #          "服務:" + srv_par    
+    text_msg = "姓名:" + name_par + "\n" + \
+               "服務:" + srv_par + "\n" + \
+               "日期:" + date_par + "\n" + \
+               "時間:" + time_par + "\n"    
+        
+    try:
+        line_bot_api.push_message(user_id,TextSendMessage(text=text_msg))
+        return text_msg
+    except LineBotApiError as e:
+        print("LineBot Error:{0}".format(e.message))
+    return jsonify(message=text_msg)
+
+@app.route("/", methods=["GET", "POST"])
+#@app.route("/", methods=["POST"])
+def callback():
+
+    if request.method == "GET":
+        return "Welcome Linebot Salon App"
+    if request.method == "POST":
+        signature = request.headers["X-Line-Signature"]
+        body = request.get_data(as_text=True)
+
+        try:
+            handler.handle(body, signature)
+        except InvalidSignatureError:
+            abort(400)
+
+        return "OK"
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    get_message = "Happy New Year 2023,"
+    get_message += event.message.text
+
+    # Send To Line
+    reply = TextSendMessage(text=f"{get_message}")
+    line_bot_api.reply_message(event.reply_token, reply)
+
+def convert_date_time(timestr):
+    #ex
+    #2023-01-03-14-25-00 => 2023/01/03 14:25:00
+    slist = list(timestr)
+    for i,c in enumerate(slist):
+        if i == 4 or i == 7:
+            slist[i] = '/'
+        elif i == 10:
+            slist[i] = ' '
+        elif i == 13 or i == 16:
+            slist[i] = ':'
+    s = ''.join(slist)    
+    print(s)
---
+++ test2.py
@@ -0,0 +1,81 @@
+import os
+from datetime import datetime
+import json
+import sys
+import csv
+
+def test_event_body():
+    json_data = {'destination': 'U7443c05aafb93ae7d746a2f87c7d396a', 'events': [{'type': 'message', 'message': {'type': 'text', 'id': '457964905652813895', 'text': '線上預約'}, 'webhookEventId': '01H20QTT83BSDC4QDAJTR4QZH9', 'deliveryContext': {'isRedelivery': False}, 'timestamp': 1685799659368, 'source': {'type': 'user', 'userId': 'U993e4bed50c6b80ac697b078fda84a01'}, 'replyToken': '8b4c16aa68ce4999ba24d929fc27d7d7', 'mode': 'active'}]}
+    print(json_data['events'][0]['replyToken'])
+    print(json_data['events'][0]['source']['userId'])
+def test_salon_info(salon_id):
+    channel_token_str = 'none'
+    channel_secret_str = 'none'
+    with open('salon_config.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            if row['salonid'] == salon_id:
+                #print(row['salonid'], row['channel_access_token'],row['channel_access_secret'],row['salon_name'])
+                return row['channel_access_token'],row['channel_access_secret']
+        return channel_token_str, channel_secret_str   
+def test_booking_info():
+    f = open('booking.json')
+    # returns JSON object as 
+    # a dictionary
+    json_text = json.load(f)
+    
+    # Iterating through the json
+    # list
+    name_str = '小美'
+    service_str = '美足'
+    date_str = '2023-11-01'
+    time_str = '14:00'
+    #new_data = {"uri":url_str}
+    for content in json_text['body']['contents']:
+        if content['type'] == 'box':
+            print(content['contents'][0]['text'])
+            if content['contents'][0]['text'] == '姓名:':
+                content['contents'][0]['text'] = content['contents'][0]['text']+name_str
+            elif content['contents'][0]['text'] == '服務:':    
+                content['contents'][0]['text'] = content['contents'][0]['text']+service_str 
+            elif content['contents'][0]['text'] == '日期:':    
+                content['contents'][0]['text'] = content['contents'][0]['text']+date_str 
+            elif content['contents'][0]['text'] == '時間:':    
+                content['contents'][0]['text'] = content['contents'][0]['text']+time_str 
+
+    json_data = json.dumps(json_text,indent=2,ensure_ascii=False).encode('utf8')
+    print(json_data.decode())
+
+    with open('output.json', 'w', encoding='utf8') as json_file:
+        json.dump(json_text,json_file,ensure_ascii=False)
+      
+
+    # Closing file
+    f.close()    
+def extract_values(obj, key):
+    """Pull all values of specified key from nested JSON."""
+    arr = []
+
+    def extract(obj, arr, key):
+        """Recursively search for values of key in JSON tree."""
+        if isinstance(obj, dict):
+            for k, v in obj.items():
+                if isinstance(v, (dict, list)):
+                    extract(v, arr, key)
+                elif k == key:
+                    arr.append(v)
+        elif isinstance(obj, list):
+            for item in obj:
+                extract(item, arr, key)
+        return arr
+
+    results = extract(obj, arr, key)
+    return results
+
+if __name__ == '__main__':
+    #token, secret = test_salon_info('420')
+    #print('\ntoken = %s, \nsecret = %s'%(token,secret))
+    test_event_body()
+
+
+
